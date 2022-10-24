212ebb8262a91e9fc6c46e2960d1591c7c1ca0ab
Adam Robson <adamray312@gmail.com>
2022-10-24 13:35:32 -0700

    add context folder and file with build

---
+++ src/context/UserContext.jsx
@@ -0,0 +1,21 @@
+import { useState, useContext, createContext } from 'react';
+import { getUser } from '../services/auth';
+
+const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const currentUser = getUser();
+  const [user, setUser] = useState(currentUser);
+
+  return <UserContext.Provider value={ { user, setUser } }>{ children }</UserContext.Provider>;
+};
+
+const useUserContext = () => {
+  const context = useContext(UserContext);
+  if (context === undefined) {
+    throw new Error('useUserContext must be used within a UserProvider');
+  }
+  return context;
+};
+
+export { UserProvider, UserContext, useUserContext };
