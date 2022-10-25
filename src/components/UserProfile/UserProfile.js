ff9a6dcf717186b27ce4a24f715a9de1fdf9a8f2
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-10-24 18:39:11 -0700

    add profile update form

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -1,3 +1,97 @@
+import { useContext, useState } from 'react';
+import { UserContext, useUserContext } from '../../context/UserContext';
+import { authUser, getUser } from '../../services/auth';
+import { updateProfile, uploadProfilePhoto } from '../../services/calls';
+import './UserProfile.css';
+
 export default function UserProfile() {
-  return <div>UserProfile</div>;
+  const user = authUser();
+  // const { user } = useContext(UserContext);
+  const [avatar, setAvatar] = useState(null);
+  const [username, setUsername] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [bio, setBio] = useState('');
+  const [city, setCity] = useState('');
+  const [projects, setProjects] = useState('');
+  const [avatarUrl, setAvatarUrl] = useState('');
+
+  const updateHandler = async (e) => {
+    e.preventDefault();
+    await updateProfile(username, firstName, lastName, email, bio, city, projects);
+    await uploadProfilePhoto();
+  };
+
+  return (
+    <>
+      <div className="profile-container">
+        <h3>User Profile</h3>
+        <form onSubmit={updateHandler} className="profile-form">
+          <h3>User{user.email}&apos;s profile</h3>
+          <div className="avatar-container" value={avatar}>
+            {avatar}
+          </div>
+          <label htmlFor="avatar">upload avatar</label>
+          <input
+            name="avatar"
+            type="file"
+            accept={'image/jpeg image/png'}
+            onChange={(e) => setAvatar(e.target.files[0])}
+          />
+          <label htmlFor="username">username</label>
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder={username}
+          />
+          <label htmlFor="first-name">first name</label>
+          <input
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            name="first-name"
+            placeholder={firstName}
+          />
+          <label htmlFor="last-name">last name</label>
+          <input
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            name="last-name"
+            placeholder={lastName}
+          />
+          <label htmlFor="email">email</label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            name="email"
+            placeholder={email}
+          />
+          <label htmlFor="bio">bio</label>
+          <input
+            value={bio}
+            onChange={(e) => setBio(e.target.value)}
+            name="bio"
+            placeholder={bio}
+          />
+          <label htmlFor="city">city</label>
+          <input
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            name="city"
+            placeholder={city}
+          />
+          <label htmlFor="projects">projects</label>
+          <input
+            value={projects}
+            onChange={(e) => setProjects(e.target.value)}
+            name="projects"
+            placeholder={projects}
+          />
+          <button type="submit" className="profile-submit" onClick={updateHandler}>
+            update profile
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
--- src/services/calls.jsx
+++ src/services/calls.jsx
@@ -48,9 +48,9 @@ export function updateTrackInRealtime(handleInsert, playlist, project_id) {
 }
 
 export async function getProject(id) {
-    // from the roster table, select a single player who has the matching id
+  // from the roster table, select a single player who has the matching id
   const response = await client.from('projects').select('*, tracks(*)').match({ id }).single();
-    // and return the response
+  // and return the response
   if (response.error) {
     throw new Error(response.error.message);
   }
@@ -64,9 +64,17 @@ export async function getProjects() {
 
 // PROFILE FETCH FNS
 
-// export async function createProfile(profile) {
-//     return await client.from('profiles').insert(profile).single();
-// }
+export async function createProfile(profile) {
+  return await client.from('profiles').insert(profile).single();
+}
+
+export async function getProfileById(id) {
+  const response = await client.from('profiles').select('*').match({ id }).single();
+  if (response.error) {
+    throw new Error(response.error.message);
+  }
+  return response.data;
+}
 
 export async function updateProfile(profile) {
   return await client.from('profiles').insert(profile).single();
