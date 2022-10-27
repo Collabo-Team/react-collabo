fb72db5aa970a38c9c8ef65c9d2d3ef09b6ea0e5
Adam Robson <adamray312@gmail.com>
2022-10-27 00:10:42 -0700

    add sign out link in Navbar to work with signing out and in. We can move it as needed

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,8 +1,13 @@
 import { NavLink } from 'react-router-dom';
-
+import { signOut } from '../../services/auth';
 import './NavBar.css';
 
 export default function NavBar() {
+
+  const handleSignOut = async () => {
+    return await signOut();
+  };
+
   return (
     <header>
       <nav>
@@ -16,6 +21,7 @@ export default function NavBar() {
           <NavLink to="/about" className="nav-link">
             About
           </NavLink>
+          <NavLink to="/auth/sign-up" onClick={ handleSignOut }>Sign Out</NavLink>
           <NavLink to="/user-profile/:id" className="nav-link">
             <img src={process.env.PUBLIC_URL + '/avatar-placeholder-circle.png'} className="nav-avatar" />
           </NavLink>
