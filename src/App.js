8f7a5b0ad3484807d2cbe2a5e8fc2f0945eac872
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2022-10-31 02:53:58 -0700

    start-project form updates state

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import UserProfile from './components/UserProfile/UserProfile';
 import StartProject from './components/StartProject/StartProject';
 import Home from './components/Home/Home';
 import NavBar from './components/NavBar/NavBar';
-import ProjectDetail from './components/ProjectDetail/ProjectDetail';
+// import ProjectDetail from './components/ProjectDetail/ProjectDetail';
 
 function App() {
   return (
@@ -22,12 +22,12 @@ function App() {
         <Route path="/user-profile/:id">
           <UserProfile/>
         </Route>
-        <Route path="/start-project/:id">
+        <Route path="/start-project">
           <StartProject/>
         </Route>
-        <Route path="/projects/:id">
+        {/* <Route path="/projects/:id">
           <ProjectDetail/>
-        </Route>
+        </Route> */}
         <Route exact path="/">
           <Home/>
         </Route>
--- src/components/StartProject/StartProject.js
+++ src/components/StartProject/StartProject.js
@@ -1,38 +1,33 @@
+import { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { useUserContext } from '../../context/UserContext';
 
 export default function StartProject() {
   const { user } = useUserContext();
+  const [projectName, setProjectName] = useState('');
+  const [genre, setGenre] = useState('');
+  const [tempo, setTempo] = useState('');
+  const [timeSignature, setTimeSignature] = useState('');
+  const [key, setKey] = useState('');
 
-  if (!user) {
-    return <Redirect to="/auth" />;
-  }
+  // if (!user) {
+  //   return <Redirect to="/auth" />;
+  // }
 
   return (
     <>
       <form>
         <label>Project Name</label>
-        <input>
-        
-        </input>
+        <input value={projectName} onChange={(e) => setProjectName(e.target.value)} />
         <label>Genre</label>
-        <input>
-        
-        </input>
+        <input value={genre} onChange={(e) => setGenre(e.target.value)} />
         <label>Tempo</label>
-        <input>
-        
-        </input>
+        <input value={tempo} onChange={(e) => setTempo(e.target.value)} />
         <label>Time Signature</label>
-        <input>
-        
-        </input>
+        <input value={timeSignature} onChange={(e) => setTimeSignature(e.target.value)} />
         <label>Key</label>
-        <input>
-        
-        </input>
-      </form>;
-
+        <input value={key} onChange={(e) => setKey(e.target.value)} />
+      </form>
     </>
   );
 }
