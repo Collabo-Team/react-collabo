cda5f28dbe55cd377886dc672807409a09a1deec
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2022-11-02 10:50:59 -0700

    home page test written and passing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import App from './App';
+import { UserProvider } from './context/UserContext';
 
-test('renders learn react link', () => {
-  // render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+test('renders home page', () => {
+
+  render(
+    <UserProvider>
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    </UserProvider>
+  );
+
+  const collaboElement = screen.getByText(/create/i);
+  expect(collaboElement).toBeInTheDocument();
 });
